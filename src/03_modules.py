fe21eb048707ac7777b6bcd425691cf60db29197
curtdragon <tealturboser@gmail.com>
2020-02-14 18:55:51 -0600

    Several done

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,35 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+#print(sys.argv)
+argvs = sys.argv
+print(*argvs, sep="\n")
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("OS Platform:", sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print("Python Version:", sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("Current process ID:", os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("Current Working Directory:", os.getcwd)
+
+
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("Current logged in user:", os.getlogin())
+
+import getpass
+
+print("Here's the real current user:", getpass.getuser())
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -9,16 +9,17 @@ for more info regarding list comprehensions.
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
-
 y = []
-
+for x in range(5):
+    y.append(x+1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for x in range(10):
+    y.append(x*x*x)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -26,14 +27,14 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [caps.upper() for caps in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+#x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
